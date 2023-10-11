d650cab07e0c047a5ffb0e7f97271c86d87d28dd
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-11 08:39:56 +0000

    .

--- pdtmcharm1/kh/kch.fish
+++
@@ -1,9 +0,0 @@
-#!/bin/fish
-/workspace/pdtmcharm1/kh/pnp.fish
-/workspace/pdtmcharm1/kh/fixrs.fish
-/workspace/pdtmcharm1/kh/st.sh
-/workspace/pdtmcharm1/kh/aryt.fish
-/workspace/pdtmcharm1/kh/tgpt.fish
-/workspace/pdtmcharm1/kh/bun.fish
-
-
