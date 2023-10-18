f74976b19b61a0706a66edccc1b3df48af51819c
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-18 17:39:15 +0000

    .

--- lnz/lis1/src/v4/v.go
+++ lnz/lis1/src/v4/v.go
@@ -41,7 +41,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		case "q", "ctrl+c":
 			return m, tea.Quit
+		
+			// This is the place where the command needs to be executed
 		case "enter":
+			tea.ExecCommand.Run(m.table.SelectedRow()[1])
 			return m, tea.Batch(
 				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
 			)
