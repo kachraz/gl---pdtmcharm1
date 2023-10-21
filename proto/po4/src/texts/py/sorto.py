f627f4d1d2ec491b857c00d74af1c217130baf2d
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-21 16:45:43 +0000

    .

--- proto/po4/src/texts/py/sorto.py
+++ proto/po4/src/texts/py/sorto.py
@@ -10,40 +10,37 @@ data = [
     {"3", "asnmap", "Go Cli/Lib map network ranges ASN"},
     {"4", "cdncheck", "ID tech with dns / ip."},
     {"5", "chaos-client", "Go client comms Chaos dataset API."},
-    {"6", "cloudlist", "Multi-cloud Assets from Cloud Providers"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    {"X", "XX", "xxxx", "github.com/projectdiscovery/x/x"},
-    
+    {"6", "cloudlist", "Multi-cloud Assets from Cloud"},
+    {"7", "dnsx", "DNS Toolkit"},
+    {"8", "httpx", "HTTP Toolkit"},
+    {"9", "interactsh-client", "Gathering Server"},
+    {"10", "interactsh-server", "Gathering Client"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
+    {"X", "XX", "xxxx"},
 ]
 
-6. cloudlist (not installed)
-7. dnsx (not installed)
-8. httpx (not installed)
-9. interactsh-client (not installed)
-10. interactsh-server (not installed)
-11. katana (not installed)
-12. mapcidr (not supported)
-13. naabu (not installed)
-14. notify (not installed)
-15. nuclei (not installed)
-16. pdtm (not installed)
-17. proxify (not installed)
-18. shuffledns (not installed)
-19. simplehttpserver (not installed)
-20. subfinder (not installed)
-21. tlsx (not installed)
-22. uncover (not installed)
-]
+
+
+
+
+# 11. katana (not installed)
+# 12. mapcidr (not supported)
+# 13. naabu (not installed)
+# 14. notify (not installed)
+# 15. nuclei (not installed)
+# 16. pdtm (not installed)
+# 17. proxify (not installed)
+# 18. shuffledns (not installed)
+# 19. simplehttpserver (not installed)
+# 20. subfinder (not installed)
+# 21. tlsx (not installed)
+# 22. uncover (not installed)
