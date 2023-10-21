86a7b77ba90620e7a1ddc74e0a583f4dbefcffe8
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-21 18:14:43 +0000

    .

---
+++ proto/po5/cmd/main.go
@@ -0,0 +1,14 @@
+/*
+main entry point of the application
+*/
+
+package main
+
+import (
+	B "po/src"
+)
+
+func main() {
+	B.Banmain()
+	B.PdtsMain()
+}
---
+++ proto/po5/src/pdts.go
@@ -0,0 +1,67 @@
+/*
+pdts.go
+This file is for
+1. Checking if pdts is installed or not
+and then it will install it
+*/
+
+package src
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	C "github.com/fatih/color"
+)
+
+func PdtsMain() {
+	PdtsChecker()
+}
+
+// Main variable that represents the program
+var (
+	programName = "pdtm"
+	programArgs = " "
+	s           = `
+go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
+`
+)
+
+// From Claude and Codeium
+func PdtsChecker() {
+
+	// Print header of program
+	// fmt.Println(Ban)
+
+	//Color Definitions
+	cr := C.New(C.FgRed).SprintFunc()
+	chg := C.New(C.FgHiGreen).SprintFunc()
+
+	// pdts command
+	cmd := exec.Command(programName, programArgs)
+
+	// Set stdout to print output
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr // This also prints out the stderr
+
+	// Settingup the logger here to print logger message also
+	logger := log.New(os.Stderr, "", 0)
+	cmd.Stderr = logger.Writer()
+
+	// Run command
+	err := cmd.Run()
+
+	if err != nil {
+		// pdts not installed
+		logger.Printf(cr("[FAIL] %s"), err)
+		fmt.Println(cr("[FAIL] PDTM - Project Discovery Open Source Tool Manager - not installed!" + s))
+	} else {
+
+		// Print success
+		fmt.Println(chg("\n[OK] Ran PDTS Successfully!...Choose Install below..."))
+		TableFuncMain()
+	}
+
+}
