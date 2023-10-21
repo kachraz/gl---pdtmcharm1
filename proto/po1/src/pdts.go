f48348510eb068d22a6b772956cee66fbbd70d8d
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-21 11:48:20 +0000

    .

--- proto/po1/src/pdts.go
+++ proto/po1/src/pdts.go
@@ -9,7 +9,6 @@ package src
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 
@@ -23,32 +22,56 @@ func PdtsMain() {
 // Main variable that represents the program
 var (
 	programName = "pdtm"
-	programArgs = "--version"
+	programArgs = " "
 	s           = `
 	go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
 	`
 )
 
+// Check if pdts is installed and then run it
+// func PdtsChecker() {
+
+// 	// Color definitions
+// 	cr := C.New(C.FgRed).SprintFunc()
+// 	cg := C.New(C.FgGreen).SprintFunc()
+
+// 	// Create a new logger with the custom color function
+// 	logger := log.New(os.Stderr, "", 0)
+// 	logger.SetFlags(0) // Remove default timestamp and file name flags
+// 	logger.SetOutput(C.Output)
+
+// 	cmd := exec.Command(programName, programArgs)
+// 	err := cmd.Run()
+// 	if err != nil {
+// 		logger.Fatalf(cr("[FAIL] %v \n %v"), cr(err), cr(s))
+// 		fmt.Printf("Install - go get ....\n")
+// 	}
+// 	logger.Println(cg("[OK] Program is installed"))
+// }
+
+// From claude
 func PdtsChecker() {
 
-	// Color definitions
+	//Color Definitions
 	cr := C.New(C.FgRed).SprintFunc()
-	cg := C.New(C.FgGreen).SprintFunc()
-	cb := C.New(C.FgBlue).SprintFunc()
+	chg := C.New(C.FgHiGreen).SprintFunc()
 
-	// Mention checking for pdts
-	fmt.Println(cb(`Checking if Pdts is installed...`))
-
-	// Create a new logger with the custom color function
-	logger := log.New(os.Stderr, "", 0)
-	logger.SetFlags(0) // Remove default timestamp and file name flags
-	logger.SetOutput(C.Output)
-
+	// pdts command
 	cmd := exec.Command(programName, programArgs)
+
+	// Set stdout to print output
+	cmd.Stdout = os.Stdout
+
+	// Run command
 	err := cmd.Run()
+
 	if err != nil {
-		logger.Fatalf(cr("[FAIL] %v \n %v"), cr(err), cr(s))
-		fmt.Printf("Install - go get ....\n")
+		// pdts not installed
+		fmt.Println(cr("Pdts not installed!"))
+	} else {
+
+		// Print success
+		fmt.Println(chg("\nRan pdts successfully!\n ---"))
 	}
-	logger.Println(cg("[OK] Pdts is Installed"))
+
 }
