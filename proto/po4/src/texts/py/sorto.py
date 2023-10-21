df9a7d8e40efe118d46ec309aed4d51c0f03c0d2
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-21 16:22:18 +0000

    .

---
+++ proto/po4/src/texts/py/sorto.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+'''
+This python file is being written to alphabetize some entries 
+'''
+
+data = [
+    {"1", "Tokyo", "Japan", "37,274,000"},
+    {"1", "aix", "AIx is a cli tool to interact with Large Language Models (LLM) APIs.", "github.com/projectdiscovery/aix"},
+    {"2", "aalterx", "", "github.com/projectdiscovery/aix"},
+
+    1. aix (latest) (0.0.3)
+2. alterx (not installed)
+3. asnmap (not installed)
+4. cdncheck (not installed)
+5. chaos-client (not installed)
+6. cloudlist (not installed)
+7. dnsx (not installed)
+8. httpx (not installed)
+9. interactsh-client (not installed)
+10. interactsh-server (not installed)
+11. katana (not installed)
+12. mapcidr (not supported)
+13. naabu (not installed)
+14. notify (not installed)
+15. nuclei (not installed)
+16. pdtm (not installed)
+17. proxify (not installed)
+18. shuffledns (not installed)
+19. simplehttpserver (not installed)
+20. subfinder (not installed)
+21. tlsx (not installed)
+22. uncover (not installed)
+]
