1257cfcefd39c8af98d713e447425caa6c0810c0
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-21 17:23:53 +0000

    .

--- proto/po4/src/table.go
+++ proto/po4/src/table.go
@@ -98,12 +98,28 @@ func TableMain() {
 	}
 
 	rows := []table.Row{
-		{"1", "aix", "AIx is a cli tool to interact with Large Language Models (LLM) APIs."},
-		{"2", "alterx", "Fast and customizable subdomain wordlist generator using DSL."},
-		{"3", "asnmap", "Go CLI and Library for quickly mapping organization network ranges using"},
-		{"4", "cdncheck", "Tool for identifying the technology associated with dns / ip network addresses."},
-		{"5", "chaos-client", "Go client to communicate with Chaos dataset API."},
-		{"6", "cloudlist", "multi-cloud tool for getting Assets from Cloud Providers"},
+		{"1", "aix", "Interact LLM APIs"},
+		{"2", "alterx", "Subdomain wordlist gen using DSL."},
+		{"3", "asnmap", "Go Cli/Lib map network ranges ASN"},
+		{"4", "cdncheck", "ID tech with dns / ip."},
+		{"5", "chaos-client", "Go client comms Chaos dataset API."},
+		{"6", "cloudlist", "Multi-cloud Assets from Cloud"},
+		{"7", "dnsx", "DNS Toolkit"},
+		{"8", "httpx", "HTTP Toolkit"},
+		{"9", "interactsh-client", "Gathering Server"},
+		{"10", "interactsh-server", "Gathering Client"},
+		{"11", "katana", "Crawl/Spider Framework"},
+		{"12", "mapcidr", "Go Cli/Lib Ops Management CIDR"},
+		{"13", "naabu", "Go Por Scanner"},
+		{"14", "notify", "Output Streamer"},
+		{"15", "nuclei", "YAML/DSL Vuln Scanner"},
+		{"16", "pdtm", "Project Discovery Tool Manager"},
+		{"17", "proxify", "Swiss Army Knife Proxy"},
+		{"18", "shuffledns", "Go massdns wrapper"},
+		{"19", "simplehttpserver", "Go Alernative to python httpserver"},
+		{"20", "subfinder", "Subdomain Discovery"},
+		{"21", "tlsx", "Tls Grabber"},
+		{"22", "uncover", "Exposed host disvoery via search"},
 	}
 
 	t := table.New(
