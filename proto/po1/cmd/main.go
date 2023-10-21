f528aba8aab7056a850fe4955cd583b60ccb89f4
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-21 11:07:18 +0000

    .

--- proto/po1/cmd/main.go
+++ proto/po1/cmd/main.go
@@ -1,17 +1,15 @@
 /*
-main entry point of the application 
+main entry point of the application
 */
 
 package main
 
 import (
-	"fmt"
 	B "po/src"
 )
 
 func main() {
-	fmt.Println("Hello World!")
 	B.Banmain()
-	B.Pdtsain()
+	B.PdtsMain()
 	B.Tablemain()
 }
--- proto/po1/src/pdts.go
+++ proto/po1/src/pdts.go
@@ -9,8 +9,46 @@ package src
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+
+	C "github.com/fatih/color"
 )
 
-func Pdtsain() {
-	fmt.Println("PDTS Print")
+func PdtsMain() {
+	PdtsChecker()
+}
+
+// Main variable that represents the program
+var (
+	programName = "pdtm"
+	programArgs = "--version"
+	s           = `
+	go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest
+	`
+)
+
+func PdtsChecker() {
+
+	// Color definitions
+	cr := C.New(C.FgRed).SprintFunc()
+	cg := C.New(C.FgGreen).SprintFunc()
+	cb := C.New(C.FgBlue).SprintFunc()
+
+	// Mention checking for pdts
+	fmt.Println(cb(`Checking if Pdts is installed...`))
+
+	// Create a new logger with the custom color function
+	logger := log.New(os.Stderr, "", 0)
+	logger.SetFlags(0) // Remove default timestamp and file name flags
+	logger.SetOutput(C.Output)
+
+	cmd := exec.Command(programName, programArgs)
+	err := cmd.Run()
+	if err != nil {
+		logger.Fatalf(cr("[FAIL] %v \n %v"), cr(err), cr(s))
+		fmt.Printf("Install - go get ....\n")
+	}
+	logger.Println(cg("[OK] Pdts is Installed"))
 }
