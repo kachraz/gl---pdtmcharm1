005813bf65f8a09cbe8ed8413e455b32d209f474
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-24 16:13:19 +0000

    .

--- proto/po7/cmd/main.go
+++ proto/po7/cmd/main.go
@@ -7,10 +7,29 @@ Main Entry Point of the application
 package main
 
 import (
+	"flag"
+	"fmt"
 	B "po/src"
 )
 
 func main() {
+	FuncHelp()
 	B.Banmain()
 	B.PdtsMain()
+}
+
+// Function help Text
+
+func FuncHelp() {
+	help := flag.Bool("help", false, "Display help message")
+	flag.Parse()
+
+	if *help {
+		displayHelp()
+		return
+	}
+}
+
+func displayHelp() {
+	fmt.Println("This is the help message.")
 }
