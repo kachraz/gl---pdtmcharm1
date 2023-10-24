bb1235f8126233760bbca1f9d0732595bc1850dd
kachraz <110726496+kachraz@users.noreply.github.com>
2023-10-24 16:18:58 +0000

    .

--- proto/po8/cmd/main.go
+++ proto/po8/cmd/main.go
@@ -7,10 +7,28 @@ Main Entry Point of the application
 package main
 
 import (
+	"fmt"
+	"os"
 	B "po/src"
 )
 
 func main() {
+
+	// When help flags executed
+
+	if len(os.Args) > 1 && os.Args[1] == "--help" {
+		printHelp()
+		return
+	}
+
+	// Main Funtion entry Point
 	B.Banmain()
 	B.PdtsMain()
+}
+
+// Help Function
+func printHelp() {
+	fmt.Println("This program shows purchase order details")
+	fmt.Println("Usage:")
+	fmt.Println("  --help   Print this help")
 }
